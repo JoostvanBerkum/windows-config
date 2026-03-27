e7d991739ee37989d69befeac13e733c8874ff6c
Joost van Berkum <64903966+JoostvanBerkum@users.noreply.github.com>
2026-03-27 12:28:16 +0100

    Change Personal recipe flag to false
    For installation purposes by colleagues. These are my personal programs.

--- start-here.ps1
+++ start-here.ps1
@@ -29,7 +29,7 @@ $InstallDutch = $true     # $true om Nederlands als extra taal toe te voegen
 $Business     = $true     # $true voor Business recept
 $OfficeSuite  = $true     # $true voor Aangepaste Office (Word/Excel/PP)
 $DevTools     = $true     # $true voor Dev-tools recept
-$Personal     = $true     # $true voor Personal recept
+$Personal     = $false     # $true voor Personal recept
 $Spotify      = $false    # $true voor Spotify recept
 
 $username = "JoostvanBerkum"
